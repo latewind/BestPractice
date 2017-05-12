b9beb69d01f163b82757b4ad982bcc60a53200ab
lsqwell <lsqwell@sina.com>
2017-05-12 08:52:10 +0800

    add puzzle

--- src/com/latewind/practice/concurrent/container/ProducerConsumer.java
+++ src/com/latewind/practice/concurrent/container/ProducerConsumer.java
@@ -7,56 +7,56 @@ import java.util.concurrent.LinkedBlockingQueue;
 import com.latewind.practice.concurrent.base.Product;
 
 public class ProducerConsumer {
-	BlockingQueue<Product> queue=new LinkedBlockingQueue<>(100);
-	public int INTERVAL_TIME=3000;
-	protected Random random =new Random();
-	public  void start(){
-		for(int i=0;i<5;i++){
-			
+	BlockingQueue<Product> queue = new LinkedBlockingQueue<>(100);
+	public int INTERVAL_TIME = 3000;
+	protected Random random = new Random();
+
+	public void start() {
+		for (int i = 0; i < 5; i++) {
+
 			new Thread(new Producer()).start();
 			new Thread(new Consumer()).start();
-			
+
 		}
-		
+
 	}
+
 	public static void main(String[] args) {
-		
+
 		new ProducerConsumer().start();
-		
+
 	}
-	class Producer implements Runnable{
+
+	class Producer implements Runnable {
 
 		@Override
 		public void run() {
-			
-			while(true){
 			try {
-				Thread.sleep(random.nextInt(INTERVAL_TIME));
-				queue.put(new Product(Thread.currentThread().getName()+"_P"));
-				System.out.println();
+				while (true) {
+					Thread.sleep(random.nextInt(INTERVAL_TIME));
+					queue.put(new Product(Thread.currentThread().getName() + "_P"));
+					System.out.println();
+				}
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			}
 		}
-		
+
 	}
-	
-	
-	class Consumer implements Runnable{
+
+	class Consumer implements Runnable {
 
 		@Override
 		public void run() {
-			while(true){
-				
-				try {
+			try {
+				while (true) {
 					Thread.sleep(random.nextInt(INTERVAL_TIME));
-					System.out.println(queue.take()+Thread.currentThread().getName()+"_C");
-				} catch (InterruptedException e) {
-					e.printStackTrace();
+					System.out.println(queue.take() + Thread.currentThread().getName() + "_C");
 				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
 		}
-		
+
 	}
 }
---
+++ src/com/latewind/practice/concurrent/puzzle/PuzzleSolver.java
@@ -0,0 +1,223 @@
+package com.latewind.practice.concurrent.puzzle;
+import java.awt.Point;
+/**
+ * @author lsqwell
+ * 递归方法走迷宫
+ * 缺点：只能走一条通道的迷宫，多条通道的会按照另一条路原路 循环 StackFlow
+ * 
+ */
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class PuzzleSolver {
+	private int[][] map={
+						{1,0,1,1,0,1},
+						{1,0,1,0,0,1},
+						{1,0,0,0,1,1},
+						{1,0,1,0,0,1},
+						{1,1,1,1,1,1}
+										};
+	private Point startPoint=new Point(0, 1, null);
+	private Point endPoint = new Point(0, 4, null);
+	private Point aimPoint=null;
+	private ConcurrentHashMap<Point, Boolean> seen=new ConcurrentHashMap<PuzzleSolver.Point, Boolean>();
+	private ExecutorService exec=Executors.newFixedThreadPool(5);
+	
+	public void sequentialShow(){
+		new Puzzle().search(startPoint);
+		aimPoint.asSolution();
+		
+	}
+	
+	public void concurrentShow(){
+		
+		new Puzzle().concurrentSearch(null);
+		
+	}
+	public void compare(){
+		HashSet<Point> points=new HashSet<PuzzleSolver.Point>();
+		Point point1=new Point(1, 1, null);
+		
+		Point point2=new Point(2, 1, null);
+		points.add(point1);
+		points.add(point2);
+		System.out.println(point1.equals(point2)+" "+points.size());
+		
+	}
+	
+	public static void main(String[] args) {
+		
+//		new PuzzleSolver().sequentialShow();
+//		new PuzzleSolver().compare();
+		new PuzzleSolver().concurrentShow();
+
+		
+	}
+	
+	
+	public class Puzzle{
+		
+		public Puzzle(){
+			
+			
+		}
+		
+		public void search(Point point){
+			if(reachTheEnd(point)){
+				aimPoint=point;
+			return ;
+			}
+				
+			List<Point> nextChildren=point.nextPoints();
+			for(Point child:nextChildren){
+				if(point.isValid(child))
+					search(child);			
+			}						
+		}
+		
+		public void concurrentSearch(Point point){
+		
+				
+				exec.execute(new SolverTask(0, 1, null));
+			
+		}
+		
+		public boolean reachTheEnd(Point point){
+			
+			if(point.getX()==endPoint.getX()&&point.getY()==endPoint.getY())
+				return true;
+			else
+				return false;
+		}
+
+		
+		
+		public class SolverTask extends Point implements Runnable{
+			
+			public SolverTask(int x, int y, Point father) {
+				super(x, y, father);
+			}
+			
+			public void run() {
+				
+				if(seen.putIfAbsent(this, true)!=null)
+					return;
+				if(reachTheEnd(this)){
+					aimPoint=this;
+					System.out.println(aimPoint);
+					exec.shutdownNow();
+					return ;
+				}
+				List<Point> nextChildren=this.nextPoints();
+				for(Point child:nextChildren){
+					if(this.isValid(child)){
+						System.out.println(Thread.currentThread().getName()+":"+child.getX()+child.getY());
+						exec.execute(new SolverTask(child.getX(), child.getY(), this) );						
+					}
+					
+				}
+				
+			}
+			
+		}
+	}
+	
+	
+	public class Point{
+		private final int x;
+		private final int y;
+		private final Point father;
+		
+		public Point(int x,int y,Point father){
+			this.father=father;
+			this.x=x;
+			this.y=y;
+		}
+		
+	public 	List<Point> nextPoints(){
+		
+			List<Point> childs=new LinkedList();
+			childs.add(new Point(x-1, y, this));
+			childs.add(new Point(x+1, y, this));
+			childs.add(new Point(x, y-1, this));
+			childs.add(new Point(x, y+1, this));
+			
+			return childs;
+		}
+		
+		boolean isValid(Point point){
+			if(isFather(point)||outOfBound(point)||isNot(point))
+				return false;
+			else
+				return true;
+		}
+		private boolean  isFather(Point point){
+			if(father==null) return false;
+			if(point.getX()==father.getX()&&point.getY()==father.getY())
+				return true;
+			else
+				return false;			
+		}
+		private boolean outOfBound(Point point){
+			if(point.getX()<0||point.getY()<0||point.getX()>map.length-1||point.getY()>map[0].length-1)
+				return true;
+			else
+				return false;
+		}
+		private boolean isNot(Point point){
+			if(map[point.getX()][point.getY()]==1)
+				return true;
+			else
+				return false;
+		}
+
+		public void asSolution(){
+			if(this.father==null)
+				return ;
+			
+				father.asSolution();
+				System.out.println(this.getX()+","+this.getY());
+		}
+	
+		public int getX(){
+			return x;
+		}
+		public int getY(){
+			
+			return y;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if(obj==null||obj.getClass()!=getClass())
+				return false;
+			Point point=(Point)obj;
+			return point.getX()==getX()&&point.getY()==getY();
+		}
+
+		@Override
+		public int hashCode() {
+			// TODO Auto-generated method stub
+			return getX()<<16+getY();
+		}
+
+		@Override
+		public String toString() {
+			return "Point [x=" + x + ", y=" + y + ", father=" + father + "]";
+		}
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
