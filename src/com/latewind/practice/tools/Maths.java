29cbe754be6a902f2a2e6291570b5a5f7f68f8e7
latewind <lsqwell@sina.com>
2017-05-14 10:23:21 +0800

    add cyclicBarrier

---
+++ src/com/latewind/practice/tools/Maths.java
@@ -0,0 +1,12 @@
+package com.latewind.practice.tools;
+
+import java.util.Random;
+
+public class Maths {
+	static Random  random= new Random();
+	public static int nextRandomInt(int range){
+		return random.nextInt(range);
+	}
+	
+
+}
