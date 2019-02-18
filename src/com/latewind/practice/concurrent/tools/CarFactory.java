18643561151f82aa333aab7f89a3e28b42aa7809
latewind <lsqwell@sina.com>
2019-02-19 07:35:49 +0800

    format

--- src/com/latewind/practice/concurrent/tools/CarFactory.java
+++ src/com/latewind/practice/concurrent/tools/CarFactory.java
@@ -9,115 +9,117 @@ import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class CarFactory {
-	final int WORKER_COUNT=2;
-	CyclicBarrier barrier;
-	CarBuilder builder=new CarBuilder();
-	 Worker[] workers =new Worker[WORKER_COUNT];
-	
-	public CarFactory() {
-		barrier=new CyclicBarrier(WORKER_COUNT,new Runnable() {
-			
-			@Override
-			public void run() {
-				
-				builder.compose();
-			}
-		});
-		
-	}
-	
-	public void start(){
-		
-		workers[0]=new Worker("buildWheel");
-		workers[1]=new Worker("buildBody");
-		for(int i=0;i<WORKER_COUNT;i++){
-			new Thread(workers[i]).start();
-		}
-	}
-	
-	public static void main(String[] args) {
-		new CarFactory().start();
-	}
+    final int WORKER_COUNT = 2;
+    CyclicBarrier barrier;
+    CarBuilder builder = new CarBuilder();
+    Worker[] workers = new Worker[WORKER_COUNT];
 
-	class Worker implements Runnable {
-		String job;
-		Method jobMethod;
-		public Worker(String job) {
-			this.job=job;
-	
-				try {
-					jobMethod=CarBuilder.class.getDeclaredMethod(job);
-				} catch (NoSuchMethodException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (SecurityException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+    public CarFactory() {
+        barrier = new CyclicBarrier(WORKER_COUNT, new Runnable() {
 
-		}
-		@Override
-		public void run() {
+            @Override
+            public void run() {
+                System.out.println("build togeter");
+                builder.compose();
+            }
+        });
 
-			try {
-				while (true) {
-					
-					Thread.sleep(Maths.nextRandomInt(5000));
-					jobMethod.invoke(builder);
-					barrier.await();
+    }
 
-				}
+    public void start() {
 
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} catch (BrokenBarrierException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IllegalArgumentException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InvocationTargetException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+        workers[0] = new Worker("buildWheel");
+        workers[1] = new Worker("buildBody");
+        for (int i = 0; i < WORKER_COUNT; i++) {
+            new Thread(workers[i]).start();
+        }
+    }
 
-	}
+    public static void main(String[] args) {
+        new CarFactory().start();
+    }
 
-	class CarBuilder {
-		AtomicInteger count=new AtomicInteger(0);
-		String wheel;
-		String body;
+    class Worker implements Runnable {
+        String job;
+        Method jobMethod;
 
-		public void buildWheel() {
-			wheel = "Big Wheel";
-			System.out.println("Build Wheel...");
-		}
+        public Worker(String job) {
+            this.job = job;
 
-		public void buildBody() {
-			body = "Cool Body";
-			System.out.println("Build Body...");
-		}
+            try {
+                jobMethod = CarBuilder.class.getDeclaredMethod(job);
+            } catch (NoSuchMethodException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (SecurityException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
 
-		public Car compose() {
-			Car newCar = new Car(body, wheel);
-			System.out.println("Success:"+count.addAndGet(1));
-			return newCar;
-		}
+        }
 
-	}
+        @Override
+        public void run() {
 
-	class Car {
-		final String wheel;
-		final String boby;
+            try {
+                while (true) {
 
-		public Car(String body, String wheel) {
-			this.wheel = wheel;
-			this.boby = body;
-		}
+                    Thread.sleep(Maths.nextRandomInt(5000));
+                    jobMethod.invoke(builder);
+                    // waiting for other
+                    barrier.await();
+                }
 
-	}
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IllegalArgumentException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    class CarBuilder {
+        AtomicInteger count = new AtomicInteger(0);
+        String wheel;
+        String body;
+
+        public void buildWheel() {
+            wheel = "Big Wheel";
+            System.out.println("Build Wheel...");
+        }
+
+        public void buildBody() {
+            body = "Cool Body";
+            System.out.println("Build Body...");
+        }
+
+        public Car compose() {
+            Car newCar = new Car(body, wheel);
+            System.out.println("Success:" + count.addAndGet(1));
+            return newCar;
+        }
+
+    }
+
+    class Car {
+        final String wheel;
+        final String boby;
+
+        public Car(String body, String wheel) {
+            this.wheel = wheel;
+            this.boby = body;
+        }
+
+    }
 
 }
