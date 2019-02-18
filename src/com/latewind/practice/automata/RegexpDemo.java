77c88797ecf67309c9d3b5ed9f5a00d3dd62c11c
latewind <lsqwell@sina.com>
2019-02-19 07:40:59 +0800

    format

--- src/com/latewind/practice/automata/RegexpDemo.java
+++ src/com/latewind/practice/automata/RegexpDemo.java
@@ -5,67 +5,67 @@ import java.util.regex.Pattern;
 
 
 public class RegexpDemo {
-	static String  patternStr ="\\s*(\\d+|[a-z_A-Z]+)|=|==";
-	static Pattern pattern=Pattern.compile(patternStr);
-	static String  testString = " 123";
-	static char[] testChars=testString.toCharArray();
-	static int index=0;
-	public static void testRegexp(){
-		
-		
-		
-	}
-	
-	
-	public String read(){
-		StringBuilder sb=new StringBuilder();
-		int c;
-		do {
-			c=getChar();
-			
-		} while (isSpace(c));
-		
-		if(c<0){
-			return null;
-		}else if(isDigit(c)){
-			
-			do {
-				sb.append((char)c);
-				c=getChar();
-			} while (isDigit(c));
-		}
-		
-		
-		return sb.toString();
-	}
-	private boolean isSpace(int c) {
-		return c==' ';
-	}
+    static String patternStr = "\\s*(\\d+|[a-z_A-Z]+)|=|==";
+    static Pattern pattern = Pattern.compile(patternStr);
+    static String testString = " 1abc23";
+    static char[] testChars = testString.toCharArray();
+    static int index = 0;
 
-	private boolean isDigit(int c){
-		
-		return '0'<=c&&c<='9';
-	}
+    public static void testRegexp() {
 
-	private int getChar(){
-		if(index<testChars.length){
-			
-			return testChars[index++];
-		}
-		
-		return -1;
-	}
-	
-	public static void main(String[] args) {
-		
-		RegexpDemo rd=new RegexpDemo();
-		String result;
-		for(;(result=rd.read())!=null;){
-			System.out.println(result);
-		}
-		
-	}
-	
-	
-	
+
+    }
+
+
+    public String read() {
+        StringBuilder sb = new StringBuilder();
+        int c;
+        do {
+            c = getChar();
+
+        } while (isSpace(c));
+
+        if (c < 0) {
+            return null;
+        } else if (isDigit(c)) {
+
+            do {
+                sb.append((char) c);
+                c = getChar();
+            } while (isDigit(c));
+        }
+
+
+        return sb.toString();
+    }
+
+    private boolean isSpace(int c) {
+        return c == ' ';
+    }
+
+    private boolean isDigit(int c) {
+
+        return '0' <= c && c <= '9';
+    }
+
+    private int getChar() {
+        if (index < testChars.length) {
+
+            return testChars[index++];
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        RegexpDemo rd = new RegexpDemo();
+        String result;
+        for (; (result = rd.read()) != null; ) {
+            System.out.println(result);
+        }
+
+    }
+
+
 }
--- src/com/latewind/practice/concurrent/puzzle/PuzzleSolver.java
+++ src/com/latewind/practice/concurrent/puzzle/PuzzleSolver.java
@@ -15,186 +15,185 @@ import java.util.concurrent.Executors;
 
 
 public class PuzzleSolver {
-	private int[][] map={
-						{1,0,1,1,0,1},
-						{1,0,1,0,0,1},
-						{1,0,0,0,1,1},
-						{1,0,1,0,0,1},
-						{1,1,1,1,1,1}
-										};
-	private Point startPoint=new Point(0, 1, null);
-	private Point endPoint = new Point(0, 4, null);
-	private Point aimPoint=null;
-	private ConcurrentHashMap<Point, Boolean> seen=new ConcurrentHashMap<PuzzleSolver.Point, Boolean>();
-	private ExecutorService exec=Executors.newFixedThreadPool(5);
+    private int[][] map = {
+            {1, 0, 1, 1, 0, 1},
+            {1, 0, 1, 0, 0, 1},
+            {1, 0, 0, 0, 1, 1},
+            {1, 0, 1, 0, 0, 1},
+            {1, 1, 1, 1, 1, 1}
+    };
+    private Point startPoint = new Point(0, 1, null);
+    private Point endPoint = new Point(0, 4, null);
+    private Point aimPoint = null;
+    private ConcurrentHashMap<Point, Boolean> seen = new ConcurrentHashMap<PuzzleSolver.Point, Boolean>();
+    private ExecutorService exec = Executors.newFixedThreadPool(5);
 
-	public void sequentialShow(){
-		new Puzzle().recursiveSearch(startPoint);
-		aimPoint.asSolution();
+    public void sequentialShow() {
+        new Puzzle().recursiveSearch(startPoint);
+        aimPoint.asSolution();
 
-	}
+    }
 
-	public void concurrentShow(){
-		new Puzzle().concurrentSearch(null);
-	}
+    public void concurrentShow() {
+        new Puzzle().concurrentSearch(null);
+    }
 
-	/**
-	 *
-	 */
-	public void compare(){
-		HashSet<Point> points=new HashSet<PuzzleSolver.Point>();
-		Point point1=new Point(1, 1, null);
-		Point point2=new Point(2, 1, null);
-		points.add(point1);
-		points.add(point2);
-		System.out.println(point1.equals(point2)+" "+points.size());
-	}
+    /**
+     *
+     */
+    public void compare() {
+        HashSet<Point> points = new HashSet<PuzzleSolver.Point>();
+        Point point1 = new Point(1, 1, null);
+        Point point2 = new Point(2, 1, null);
+        points.add(point1);
+        points.add(point2);
+        System.out.println(point1.equals(point2) + " " + points.size());
+    }
 
-	public static void main(String[] args) {
-		new PuzzleSolver().sequentialShow();
+    public static void main(String[] args) {
+        new PuzzleSolver().sequentialShow();
 //		new PuzzleSolver().compare();
-		new PuzzleSolver().concurrentShow();
+        new PuzzleSolver().concurrentShow();
 
 
-	}
+    }
 
 
-	public class Puzzle{
-		
-		public Puzzle(){
+    public class Puzzle {
 
-		}
-		
-		public void recursiveSearch(Point point){
-			if(reachTheEnd(point)){
-				aimPoint=point;
-			return ;
-			}
-			List<Point> nextChildren=point.nextPoints();
-			for(Point child:nextChildren){
-				if(point.isValid(child))
-					recursiveSearch(child);
-			}						
-		}
-		
-		public void concurrentSearch(Point point){
-			exec.execute(new SolverTask(0, 1, null));
-		}
-		
-		public boolean reachTheEnd(Point point){
+        public Puzzle() {
+
+        }
+
+        public void recursiveSearch(Point point) {
+            if (reachTheEnd(point)) {
+                aimPoint = point;
+                return;
+            }
+            List<Point> nextChildren = point.nextPoints();
+            for (Point child : nextChildren) {
+                if (point.isValid(child))
+                    recursiveSearch(child);
+            }
+        }
+
+        public void concurrentSearch(Point point) {
+            exec.execute(new SolverTask(0, 1, null));
+        }
+
+        public boolean reachTheEnd(Point point) {
             return point.getX() == endPoint.getX() && point.getY() == endPoint.getY();
-		}
+        }
 
-		
-		
-		public class SolverTask extends Point implements Runnable{
-			
-			public SolverTask(int x, int y, Point father) {
-				super(x, y, father);
-			}
-			
-			public void run() {
-				
-				if(seen.putIfAbsent(this, true)!=null)
-					return;
-				if(reachTheEnd(this)){
-					aimPoint=this;
-					System.out.println(aimPoint);
-					exec.shutdownNow();
-					return ;
-				}
-				List<Point> nextChildren=this.nextPoints();
-				for(Point child:nextChildren){
-					if(this.isValid(child)){
-						System.out.println(Thread.currentThread().getName()+":"+child.getX()+child.getY());
-						exec.execute(new SolverTask(child.getX(), child.getY(), this) );						
-					}
-					
-				}
-				
-			}
-			
-		}
-	}
-	
-	
-	public class Point{
-		private final int x;
-		private final int y;
-		private final Point father;
-		
-		public Point(int x,int y,Point father){
-			this.father=father;
-			this.x=x;
-			this.y=y;
-		}
-		
-	public 	List<Point> nextPoints(){
-		
-			List<Point> childs=new LinkedList();
-			childs.add(new Point(x-1, y, this));
-			childs.add(new Point(x+1, y, this));
-			childs.add(new Point(x, y-1, this));
-			childs.add(new Point(x, y+1, this));
-			
-			return childs;
-		}
-		
-		boolean isValid(Point point){
+
+        public class SolverTask extends Point implements Runnable {
+
+            public SolverTask(int x, int y, Point father) {
+                super(x, y, father);
+            }
+
+            public void run() {
+
+                if (seen.putIfAbsent(this, true) != null)
+                    return;
+                if (reachTheEnd(this)) {
+                    aimPoint = this;
+                    System.out.println(aimPoint);
+                    exec.shutdownNow();
+                    return;
+                }
+                List<Point> nextChildren = this.nextPoints();
+                for (Point child : nextChildren) {
+                    if (this.isValid(child)) {
+                        System.out.println(Thread.currentThread().getName() + ":" + child.getX() + child.getY());
+                        exec.execute(new SolverTask(child.getX(), child.getY(), this));
+                    }
+
+                }
+
+            }
+
+        }
+    }
+
+
+    public class Point {
+        private final int x;
+        private final int y;
+        private final Point father;
+
+        public Point(int x, int y, Point father) {
+            this.father = father;
+            this.x = x;
+            this.y = y;
+        }
+
+        public List<Point> nextPoints() {
+
+            List<Point> childs = new LinkedList();
+            childs.add(new Point(x - 1, y, this));
+            childs.add(new Point(x + 1, y, this));
+            childs.add(new Point(x, y - 1, this));
+            childs.add(new Point(x, y + 1, this));
+
+            return childs;
+        }
+
+        boolean isValid(Point point) {
             return !(isFather(point) || outOfBound(point) || isNot(point));
-		}
-		private boolean  isFather(Point point){
-			if(father==null) return false;
+        }
+
+        private boolean isFather(Point point) {
+            if (father == null) return false;
             return point.getX() == father.getX() && point.getY() == father.getY();
-		}
-		private boolean outOfBound(Point point){
+        }
+
+        private boolean outOfBound(Point point) {
             return point.getX() < 0 || point.getY() < 0 || point.getX() > map.length - 1 || point.getY() > map[0].length - 1;
-		}
-		private boolean isNot(Point point){
+        }
+
+        private boolean isNot(Point point) {
             return map[point.getX()][point.getY()] == 1;
-		}
+        }
 
-		public void asSolution(){
-			if(this.father==null)
-				return ;
-			
-				father.asSolution();
-				System.out.println(this.getX()+","+this.getY());
-		}
-	
-		public int getX(){
-			return x;
-		}
-		public int getY(){
-			
-			return y;
-		}
+        public void asSolution() {
+            if (this.father == null)
+                return;
 
-		@Override
-		public boolean equals(Object obj) {
-			if(obj==null||obj.getClass()!=getClass())
-				return false;
-			Point point=(Point)obj;
-			return point.getX()==getX()&&point.getY()==getY();
-		}
+            father.asSolution();
+            System.out.println(this.getX() + "," + this.getY());
+        }
 
-		@Override
-		public int hashCode() {
-			// TODO Auto-generated method stub
-			return getX()<<16+getY();
-		}
+        public int getX() {
+            return x;
+        }
 
-		@Override
-		public String toString() {
-			return "Point [x=" + x + ", y=" + y + ", father=" + father + "]";
-		}
-		
-		
-		
-		
-		
-	}
-	
-	
+        public int getY() {
+
+            return y;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null || obj.getClass() != getClass())
+                return false;
+            Point point = (Point) obj;
+            return point.getX() == getX() && point.getY() == getY();
+        }
+
+        @Override
+        public int hashCode() {
+            // TODO Auto-generated method stub
+            return getX() << 16 + getY();
+        }
+
+        @Override
+        public String toString() {
+            return "Point [x=" + x + ", y=" + y + ", father=" + father + "]";
+        }
+
+
+    }
+
 
 }
